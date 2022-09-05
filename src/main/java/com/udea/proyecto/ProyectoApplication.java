5aa790fbd057e8a430a9b51fae9b3049911f58cb
GERMAN8901 <106921625+GERMAN8901@users.noreply.github.com>
2022-09-04 22:56:05 -0500

    se crea la rama Tarea_German_2

--- src/main/java/com/udea/proyecto/ProyectoApplication.java
+++ src/main/java/com/udea/proyecto/ProyectoApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProyectoApplication {
 
+
 	public static void main(String[] args) {
 
 		SpringApplication.run(ProyectoApplication.class, args);
