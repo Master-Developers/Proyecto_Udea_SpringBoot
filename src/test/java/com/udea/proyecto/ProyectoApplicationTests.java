d4f161104a8531f9c73b2ba4404db4f6e4de4384
GERMAN8901 <106921625+GERMAN8901@users.noreply.github.com>
2022-09-05 22:07:39 -0500

    Dependencias Maven para los Test Integrados en Spring Boot

--- src/test/java/com/udea/proyecto/ProyectoApplicationTests.java
+++ src/test/java/com/udea/proyecto/ProyectoApplicationTests.java
@@ -1,15 +1,25 @@
 package com.udea.proyecto;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 class ProyectoApplicationTests {
 
-	@Test
-	public void test1 () {
+	@ExtendWith(SpringExtension.class)
+	@SpringBootTest
+	@AutoConfigureMockMvc
+	public class BookControllerIT {
+		@Autowired
+		private MockMvc mockMvc;
 
 
 	}
 
 }
+
